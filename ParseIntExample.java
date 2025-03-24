4a54a50619fefe97ecb99f5290834cc0f51d976d
deepikakethavath8688 <deepikakethavath2@gmail.com>
2025-03-24 12:38:47 +0530

    Add files via upload

---
+++ ParseIntExample.java
@@ -0,0 +1,8 @@
+class ParseIntExample {  
+    public static void main(String[] args) {    
+        String str = "149";  
+        int num = Integer.parseInt(str);  
+        System.out.println(num + 3); // This will print 152  
+    }    
+}  
+
